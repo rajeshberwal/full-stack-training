3086892554de6cb61d78d1a1f2df4571ed164b79
Rajesh Kumar <irajeshberwal@gmail.com>
2022-04-12 13:53:12 +0530

    day 30 files

---
+++ day-30-react-int/steps/src/mainapp.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import UsersComp from "./user.component";
+
+class MainApp extends Component{
+    render(){
+        return <div>
+                    <h1>Welcome to My Application</h1>
+                    <hr />
+                    <UsersComp />
+                </div>
+    }
+}
+
+export default MainApp
